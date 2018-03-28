6395f3467af887ef71595a9c5c793bb1764b0e71
mjkushman <31631046+mjkushman@users.noreply.github.com>
2018-03-28 16:36:21 -0700

    Fixed the reply texts. Also fixed the bot summoning command so that exclamationpoint command works. Also fixed some issues with pulling coinscore for users that had none.

--- wholesomecoin.py
+++ wholesomecoin.py
@@ -6,22 +6,21 @@
 import praw, pprint, datetime, sqlite3, string, time, sys
 
 
-
 conn = sqlite3.connect('wholesomeCoin.db')
 c = conn.cursor()
 
 
-SEARCHQ = 'you' #must be lowercase
-REPLY_TEXT= '\nHere\'s +1 WholesomeCoin for u/{}!\n\nCurrent wholesome coinage: **{}**.\n\n*****\n\n*^Bleep ^bloop. ^How\'s ^your ^day?*'
-HOTSUBSTORUN = 0  #number of HOT submissions to check in each subreddit
-NEWSUBSTORUN = 20  #number of NEW submissions to check in each subreddit
+SEARCHQ = '!secretkeyword' #must be lowercase
+AWARD_TEXT= '\n+1 WholesomeCoin for u/{}!\nCurrent wholesome coinage: **{}**.\n\n*****\n\n*^Bleep ^bloop. ^How ^are ^you? ^I\'m ^a ^work ^in ^progress. ^| [^message ^me](https://reddit.com/message/compose/?to=wholesomecoinbot)^| [^Info ^placeholder](https://google.com) ^|*'
+HOT_SUBSTORUN = 20  #number of HOT submissions to check in each subreddit
+NEW_SUBSTORUN = 20  #number of NEW submissions to check in each subreddit
 #SUBREDDITS_WHITELIST = ['AskReddit','testingground4bots','todayilearned']
 SUBREDDITS_WHITELIST = ['testingground4bots']
-DENY_TEXT= 'Hey don\'t do that!\nI\'m taking away half your WholesomeCoins :(\n\n*****\n\n*^Bleep ^bloop. ^How\'s ^your ^day?*'
+DENY_TEXT= 'Hey don\'t do that!\nI\'m taking away half your WholesomeCoins :(\n\n*****\n\n*^Bleep ^bloop. ^How ^are ^you? ^I\'m ^a ^work ^in ^progress. ^| [^message ^me](https://reddit.com/message/compose/?to=wholesomecoinbot)^| [^Info ^placeholder](https://google.com) ^|*'
 coinsGiven = 0 #keeps track of how many coins given out per script run
 coinUp = 1 #how many coins a user should get
-lastPostTime = int(time.time())
-POSTINTERVAL = 3 #how long to wait between replies, in seconds
+POSTINTERVAL = 60 #how long to wait between replies, in seconds
+lastPostTime = POSTINTERVAL
 REPLYLIMIT = 5	 #max number of replies to send in a given submission.
 
 #Creates the table to keep track of everyone's coins
@@ -76,13 +75,6 @@ def createView():
 		GROUP BY wholesome_users.username
 		''')
 
-#def forestExplorer(redditObject):
-#	commentAuthor = redditObject.author #define the object's author
-#	parentComment = redditObject.parent() #define the object's parent
-#	parentCommentAuthor = parentComment.author #define the object's parent's author
-#	bodyCopy = redditObject.body.lower() #make text lowercase
-#	qFinder(redditObject)
-#	return
 
 def isObjectValid(redditObject):
 	if redditObject.author == None:
@@ -100,7 +92,7 @@ def isObjectValid(redditObject):
 def qFinder(redditObject):
 	#print('STARTING qFinder')
 	global repliesSent
-	copy = redditObject.body.lower().translate(str.maketrans('','',string.punctuation)) #removes punctuation
+	copy = redditObject.body.lower().translate(str.maketrans('','',string.punctuation[1:])) #removes punctuation EXCEPT FOR ! which is at index 0 of string.punctuation
 	if SEARCHQ in copy.split():
 		#print('object:')
 		#pprint.pprint(vars(redditObject))
@@ -110,19 +102,23 @@ def qFinder(redditObject):
 
 		if redditObject.author == redditObject.parent().author: #Will TAKE coins if the user tried to reward themselves
 			
-			c.execute('SELECT comment_id FROM wholesome_coining')
-			coinData = c.fetchall() #coinData is a list of touples
-			commentIdList = [t[0] for t in coinData]
-			if redditObject.id not in commentIdList:
+			#c.execute('SELECT comment_id FROM wholesome_coining')
+			#coinData = c.fetchall() #coinData is a list of touples
+			#commentIdList = [t[0] for t in coinData]
+			#if redditObject.id not in commentIdList:
 				#pprint.pprint(vars(redditObject))
 				
 				#pprint.pprint(vars(redditObject.parent()))
-				coinScore = coinPenalty(redditObject)
-				print(DENY_TEXT)
-				print('Pretend Reply Sent! (- coins)')
+			coinScore = coinPenalty(redditObject)
+				
+			sendReply(redditObject, coinScore, DENY_TEXT)
+
+			#remove ---- below
+				#print(DENY_TEXT)
+				#print('Pretend Reply Sent! (- coins)')
 				#sendReply(redditObject, parent, coinScore)
-				repliesSent[redditObject._submission] += 1
-				#repliesSent +=1
+				#repliesSent[redditObject._submission] += 1
+			#------- til here	
 
 		elif redditObject.author != redditObject.parent().author and isObjectValid(redditObject) == True:			 #Will GIVE coins
 
@@ -131,27 +127,58 @@ def qFinder(redditObject):
 							
 			
 				
-			sendReply(redditObject, coinScore)
+			sendReply(redditObject, coinScore, AWARD_TEXT)
 	elif SEARCHQ not in copy.split():
 		pass
 	return
 		
 
 
-def coinGiver(redditObject):
-	#print('STARTING coinGiver')
-#1- record the coining action in coining table
-	coiningTracker(redditObject, coinUp)
-#2 - IF the parent is not already tracked in users table, add to table
+def coinGiver(redditObject): #awards Coins for users who are wholesome. Opposite of coinPenalty
+	
+
+#1 - IF the parent is not already tracked in users table, add to table
 	wholesomeUserTracker(redditObject)
-	#coinScore = 0
+
+#2- record the coining action in coining table
+	coiningTracker(redditObject, coinUp)
+
+#3 - Get user's coinScore	
 	c.execute('SELECT total_coins FROM wholesome_score WHERE username=(?)',(redditObject.parent().author.name,))
 	coinScore = c.fetchone()
 	coinScore = round(coinScore[0],2)
 	return coinScore
 
-def sendReply(redditObject, coinScore):
-	#print('starting SENDREPLY')
+
+
+def coinPenalty(redditObject): #deducts Coins for users who try to abuse. Opposite of coinGiver
+	global coinsGiven #TODO: remove for production
+	coinScore = 0
+#1 - IF the parent is not already tracked in users table, add to table
+	wholesomeUserTracker(redditObject)
+
+#2- Get user's coinScore and define the penalty
+	c.execute('SELECT total_coins FROM wholesome_score WHERE username=(?)',(redditObject.parent().author.name,))
+	coinScore = c.fetchone()
+	if coinScore == None or coinScore[0] == 0:
+		print('if triggered')
+		penalty = 0
+		coinScore = 0.0
+	else:
+		print('elif2 triggered')
+		penalty = (coinScore[0]/2)*-1
+		coinScore = round(coinScore[0],2)
+		
+	print('PENALTY:', penalty) #remove for prod
+
+#3- record the coining action in coining table.
+	coiningTracker(redditObject, penalty) #uses penalty instead of award
+	
+	return coinScore
+
+
+def sendReply(redditObject, coinScore, replyText): #replies to the post
+	
 	global lastPostTime
 	global repliesSent
 	now = int(time.time())
@@ -161,10 +188,11 @@ def sendReply(redditObject, coinScore):
 		if now - lastPostTime > POSTINTERVAL: #requires the last post to be at least 6 seconds ago
 			c.execute('SELECT replied_to FROM wholesome_coining WHERE comment_id =(?)', (redditObject.id,))
 			coinData = c.fetchone() #coinData is a touple
-			if	coinData[0] == False: #checks to see if this one has been replied to or not.
+			if	coinData[0] == False: #checks to see if this comment has been replied to or not.
 				#Uncommet the next line to enable replies !!GOES LIVE!!
-				#redditObject.reply(REPLY_TEXT.format(redditObject.parent().author, coinScore))
-				print('Pretend Reply Sent! (+ coin)')
+				redditObject.reply(replyText.format(redditObject.parent().author, coinScore))
+				print('Pretend Reply Sent!')
+				print(replyText)
 				lastPostTime = int(time.time())
 				repliesSent[redditObject._submission] += 1
 				c.execute('''UPDATE wholesome_coining
@@ -174,36 +202,10 @@ def sendReply(redditObject, coinScore):
 			else:
 				print('Didn\'t send reply because already replied')
 		else:
-			print('Didn\'t send reply because Now - lastPostTime =',(now - lastPostTime))
-
-def coinPenalty(redditObject):
-	#print('STARTING coinPenalty')
-	global coinsGiven #TODO: remove for production
-	coinScore = 0
-#1 - IF the parent is not already tracked in users table, add to table
-	parent = redditObject.parent().author.name
-	c.execute('SELECT username FROM wholesome_users')
-	coinData = c.fetchall() #coinData is a list of touples
-	authorsList = [t[0] for t in coinData]
-	if parent not in authorsList:
-		wholesomeUserTracker(redditObject)
-		coinScore = 0
-
-#2- record the coining action in coining table. In this case, 0 coins were awarded.
-#2.1 define the penalty
-
-	c.execute('SELECT username, total_coins FROM wholesome_score WHERE username = (?)', (parent,))
-	coinData = c.fetchall()
-	
-	if len(coinData) > 0:
-		penalty = ((coinData[0][1])/2)*-1
+			print('Didn\'t send reply because (Now-lastPostTime)=',(now - lastPostTime))
 	else:
-		penalty = 0
-	print('PENALTY:', penalty)
-	coiningTracker(redditObject, penalty)
+		print('Didn\'t reply because already 5+ replies in this submission')
 
-	
-	return
 
 #=============POGRAM START=========
 
@@ -236,7 +238,7 @@ while True:
 		subreddit = reddit.subreddit(subreddit)
 		#for submission in subreddit.stream.submissions(): #TODO: use this version for stream of new
 # ----------program, for HOT-------------------------------------------------------
-		for subIndex, submission in enumerate(subreddit.hot(limit=HOTSUBSTORUN)):
+		for subIndex, submission in enumerate(subreddit.hot(limit=HOT_SUBSTORUN)):
 			#repliesSent = 0 #resets replies to 0. track replies per submission
 			#repliesSent[submission] =0
 			print('===starting "{}" ==='.format(submission.title))
@@ -250,7 +252,7 @@ while True:
 			#print('pretend replies sent', repliesSent[submission])
 			subCount = subIndex +1	
 # ----------same, but for new-----------------------------------------------------
-		for subIndex, submission in enumerate(subreddit.new(limit=NEWSUBSTORUN)):
+		for subIndex, submission in enumerate(subreddit.new(limit=NEW_SUBSTORUN)):
 			#repliesSent[submission] = 0 #track replies per submission
 			print('===starting "{}" ==='.format(submission.title))
 
@@ -264,11 +266,11 @@ while True:
 			subCount = subIndex +1	
 # --------------------------------------------------------------------------------
 		print('Scanned the following submissions in HOT:\n')
-		for index, submission in enumerate(subreddit.hot(limit=HOTSUBSTORUN)):
+		for index, submission in enumerate(subreddit.hot(limit=HOT_SUBSTORUN)):
 			print(index+1, submission.title)
 #------------------		
 		print('Scanned the following submissions in NEW:\n')
-		for index, submission in enumerate(subreddit.new(limit=NEWSUBSTORUN)):
+		for index, submission in enumerate(subreddit.new(limit=NEW_SUBSTORUN)):
 			print(index+1, submission.title)
 #------------------
 		print('\n!!Completed {} submissions in {} and gave out {} WholesomeCoins!!'.format(subCount, subreddit, coinsGiven))
